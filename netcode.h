a1f92e54a859eea0e055add8623cab12e8ce2468
mpope9 <mpope@skillz.com>
2017-09-28 11:09:35 -0700

    [[#151394226]] Bahh one line missed.

--- netcode.h
+++ netcode.h
@@ -105,7 +105,7 @@ typedef struct skillz_match_t
 {
     uint64_t 		skillz_match_id;		/* key */
     uint64_t 		clients_in_match[SKILLZ_MAX_CLIENTS_PER_MATCH];
-    int 			num_clients_in_match;
+    int 		num_clients_in_match;
     UT_hash_handle 	hh;						/* Makes this structure hashable!! */
 
 } skillz_match_t;
